a58b1439c8831e2f222abe1a604daf4e5d8dcdb0
Susan Acharya <susanacharya.sp@gmail.com>
2025-07-17 21:04:29 +0545

    Update settings.py

--- student_mgmt/student_mgmt/settings.py
+++ student_mgmt/student_mgmt/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#gjg*$q9ranh73@o*e3@y0s$jsjme+1ll4ofeb25(!ps1dyu8a'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
