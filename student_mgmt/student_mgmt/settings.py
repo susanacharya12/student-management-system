e03da87fdac2b27d8c3ba3e8b6083da19e87b208
Susan Acharya <susanacharya.sp@gmail.com>
2025-07-17 21:05:13 +0545

    Update settings.py

--- student_mgmt/student_mgmt/settings.py
+++ student_mgmt/student_mgmt/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
